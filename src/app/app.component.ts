595441a4852794e80aa363a5c8e09fd147b410c0
mark.baje <mark.baje@sanyutechnos-ph.com>
2024-04-18 08:43:59 +0000

    Added recipe component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IngredientsComponent } from './ingredients/ingredients.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IngredientsComponent],
+  imports: [RouterOutlet, IngredientsComponent, RecipesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'cookme';
+  title = 'CookMe';
 }
