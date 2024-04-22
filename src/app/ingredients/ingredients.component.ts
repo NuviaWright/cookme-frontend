d33a33b0e37802e370135ef812cd673b7e28bc67
mark.baje <mark.baje@sanyutechnos-ph.com>
2024-04-22 03:47:41 +0000

    Added error message

--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -3,27 +3,36 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Ingredients } from '../ingredient';
 import { RecipeService } from '../recipe.service';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-ingredients',
   standalone: true,
-  imports: [FormsModule, CommonModule, HttpClientModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './ingredients.component.html',
   styleUrl: './ingredients.component.css',
 })
 export class IngredientsComponent {
   ingredients: Array<string> = [];
   ingredient = new Ingredients('');
+  error = false;
+  errorMessage: string = '';
 
   constructor(private recipeService: RecipeService) {}
 
   addIngredient() {
     if (this.ingredient.name == '') return;
 
-    this.ingredients.push(this.ingredient.name);
     // const recipes = this.recipeService.recipeFind(this.ingredient.name);
-    console.log(this.recipeService.fetchRecipe(this.ingredient.name));
+    this.recipeService.fetchRecipe(this.ingredient.name).then((res) => {
+      console.log(res);
+      if (res?.code == 'NG') {
+        this.error = true;
+        this.errorMessage = res.message;
+        return;
+      }
+
+      this.ingredients.push(this.ingredient.name);
+    });
 
     this.ingredient.name = '';
   }
