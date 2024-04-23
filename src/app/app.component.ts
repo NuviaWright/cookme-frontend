e8600e10253486766b4af21b8a167ea2a6b63b1d
mark.baje <mark.baje@sanyutechnos-ph.com>
2024-04-23 08:36:53 +0000

    Modified routing for home page and food page. Added Food component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,13 @@
-import { Component, Input } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { IngredientsComponent } from './ingredients/ingredients.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeSearchRes } from './recipe-search-res';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IngredientsComponent, RecipesComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'CookMe';
-  recipes: RecipeSearchRes = {
-    response: [],
-    code: '',
-    message: '',
-  };
-
-  onRecipes(res: any) {
-    this.recipes = res;
-  }
 }
