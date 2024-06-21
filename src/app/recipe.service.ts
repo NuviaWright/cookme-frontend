06e875fed854b37618d93857414cb8d230901b42
NuviaWright <markdanielbaje@gmail.com>
2024-06-21 06:48:53 +0000

    Changed from fetch() to built-in HttpClient

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
 import { RecipeSearchRes } from './recipe-search-res';
-import { catchError, map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -17,51 +16,50 @@ export class RecipeService {
 
   constructor(private http: HttpClient) {}
 
-  recipeFind(ingredient: string): Observable<RecipeSearchRes> {
-    const urlRecipe = `${this.urlRecipe}/${ingredient}`;
-    return this.http.get<RecipeSearchRes>(urlRecipe).pipe(
-      tap((_) => this.log('fetched data')),
-      catchError(this.handleError<RecipeSearchRes>('recipeFind'))
+  async fetchRecipe(ingredient: string): Promise<RecipeSearchRes> {
+    const res = await fetch(`${this.urlRecipe}/find/${ingredient}`, {
+      method: 'GET',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+    });
+    return (await res.json()) ?? '';
+  }
+
+  httpRecipe(ingredient: string): Observable<RecipeSearchRes> {
+    return this.http.get<RecipeSearchRes>(
+      `${this.urlRecipe}/find/${ingredient}`
     );
   }
 
-  async fetchRecipe(ingredient: string): Promise<RecipeSearchRes> {
-    const res = await fetch(`${this.urlRecipe}/find/${ingredient}`);
+  async fetchMeal(mealId: number): Promise<RecipeSearchRes> {
+    const res = await fetch(`${this.urlRecipe}/meal/${mealId}`, {
+      method: 'GET',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        accept: 'application/json',
+      },
+    });
     return (await res.json()) ?? '';
   }
 
-  async fetchMeal(mealId: number): Promise<RecipeSearchRes> {
-    const res = await fetch(`${this.urlRecipe}/meal/${mealId}`);
-    return (await res.json()) ?? '';
+  httpMeal(mealId: number): Observable<RecipeSearchRes> {
+    return this.http.get<RecipeSearchRes>(`${this.urlRecipe}/meal/${mealId}`);
   }
 
   async fetchIngredient(): Promise<RecipeSearchRes> {
-    const res = await fetch(`${this.urlIngredient}/list`);
+    const res = await fetch(`${this.urlIngredient}/list`, {
+      method: 'GET',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+    });
     return (await res.json()) ?? '';
   }
 
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   *
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
-
-  /** Log a HeroService message with the MessageService */
-  private log(message: string) {
-    console.log(message);
+  httpIngredient(): Observable<RecipeSearchRes> {
+    return this.http.get<RecipeSearchRes>(`${this.urlIngredient}/list`);
   }
 }
